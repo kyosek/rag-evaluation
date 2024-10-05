67e4bdd79976b75abaa5d74b0b8c6451a2b94d5d
kyosuke <kyosuke1029@icloud.com>
2024-10-05 09:27:18 +0000

    add questions

--- auto-rag-eval/ExamGenerator/question_generator.py
+++ auto-rag-eval/ExamGenerator/question_generator.py
@@ -83,14 +83,17 @@ class BatchExamGenerator:
 
         # Split the data into batches
         batches = [data[i:i + self.batch_size]
-                   for i in range(0, len(data), self.batch_size)]
+                #    for i in range(0, len(data), self.batch_size)]
+                    for i in range(35 * self.batch_size, len(data), self.batch_size)]  # resuming from interrupted process
 
         start_time = datetime.fromtimestamp(
             time.time()).strftime('%Y%m%d%H')
 
         # try:
 
+        # for batch_index, batch in enumerate(batches):
         for batch_index, batch in enumerate(batches):
+            batch_index += 35  # resuming from interrupted process
 
             logger.error(f"Running batch {batch_index}.")
             if len(self.model_list) > 1:
