f07fe9629d473eaa68fd9647f1a6772f63d1eed9
Kyosuke <kyosuke1029@icloud.com>
2024-10-19 23:03:08 +0100

    bm25 rag

--- auto-rag-eval/ExamEvaluator/solve_exam_rag.py
+++ auto-rag-eval/ExamEvaluator/solve_exam_rag.py
@@ -21,7 +21,6 @@ def extract_text(data: Dict) -> List[str]:
 
 
 def load_corpus(data_path: str):
-    # corpus_file = read_jsonl(data_path)
     loader = JSONLoader(
         file_path=data_path,
         jq_schema='.[]',
@@ -40,7 +39,7 @@ def load_exam(file_path):
 def get_retriever(retriever_type):
     if retriever_type == "bm25":
         bm25_retriever = BM25Retriever.from_documents(texts)
-        bm25_retriever.k = 5  # Number of documents to retrieve
+        bm25_retriever.k = 1  # Number of documents to retrieve
         return bm25_retriever
     vectorstore = Chroma.from_documents(texts, EMBEDDINGS)
     if retriever_type == "dpr":
@@ -58,20 +57,39 @@ def get_retriever(retriever_type):
         raise ValueError("Invalid retriever type")
 
 
-def generate_answer(model, question: str, choices: List[str], retriever) -> str:
-    contexts = retriever.invoke(question)
+def retrieve_context(question: str, retriever):
+    docs = retriever.invoke(question)
 
+    retrieved_docs_text = [
+        doc.page_content for doc in docs
+    ]
+    context = "\nRetrieved contexts:\n"
+    context += "".join(
+        [f"Context {i}:::\n{doc}" for i, doc in enumerate(retrieved_docs_text)]
+    )
+
+    return context, retrieved_docs_text
+
+
+def generate_answer(model, question: str, choices: List[str], retriever) -> str:
+    contexts = retrieve_context(question, retriever)
     prompt = f"Question: {question}\n\nChoices:\n"
     for i, choice in enumerate(choices):
-        prompt += f"{chr(65 + i)}) {choice}\n"
+        prompt += f"{choice}\n"
     prompt += f"""
     \nYou are a student that is solving the exam.
     Given contexts, please provide the letter (A, B, C, or D) of the correct answer based on the retrieved information.
     Context:
     {contexts}\n
     The response must follow the response format:
-    Response format example:
+    - Return only one letter (A, B, C, or D)
+    - No period or anything else at the end of the sentence\n
+    Response format example 1:
     A
+    Response format example 2:
+    C
+    Response format example 3:
+    D
     """
 
     response = model.invoke(prompt)
@@ -83,13 +101,16 @@ def evaluate_performance(exam: List[Dict], results: List[str]) -> float:
     return correct / len(exam)
 
 
-def run_rag_exam(model_name: str, task_name: str, exam_file: str, retriever_type: str):
+def run_rag_exam(model_name: str, task_name: str, exam_file: str, retriever):
     exam = load_exam(exam_file)
 
     results = []
     for question in tqdm(exam, desc="Processing questions", unit="question"):
-        answer = generate_answer(LLM, question["question"], question["choices"], retriever_type)
-        results.append(answer)
+        try:
+            answer = generate_answer(LLM, question["question"], question["choices"], retriever)
+            results.append(answer)
+        except:
+            pass
 
     accuracy = evaluate_performance(exam, results)
 
@@ -104,11 +125,11 @@ def run_rag_exam(model_name: str, task_name: str, exam_file: str, retriever_type
             }
         )
 
-    with open(f"Data/{task_name}/ExamResults/exam_results_{model_name}_{task_name}_{retriever_type}.json", "w") as f:
+    with open(f"Data/{task_name}/ExamResults/exam_results_{model_name}_{task_name}_{retriever}.json", "w") as f:
         json.dump(output, f, indent=2)
 
     print(f"Exam completed. Accuracy: {accuracy:.2%}")
-    print(f"Results saved to exam_results_{model_name}_{task_name}_{retriever_type}.json")
+    print(f"Results saved to exam_results_{model_name}_{task_name}_{retriever}.json")
 
 
 if __name__ == "__main__":
@@ -122,7 +143,7 @@ if __name__ == "__main__":
 
     documents = load_corpus(f"Data/{task_name}/KnowledgeCorpus/main/{corpus_file_name}")
 
-    text_splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=4500, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
     retriever = get_retriever(retriever_type)
 
