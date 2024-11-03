cd739f9d1f2ab2c9c6be527c554e03a7e662aa35
kyosek <kyosuke1029@icloud.com>
2024-11-03 13:25:06 +0000

    adding l3 exam secfiling

--- auto-rag-eval/ExamEvaluator/exam_sampler.py
+++ auto-rag-eval/ExamEvaluator/exam_sampler.py
@@ -39,7 +39,7 @@ def sample_json_entries(input_file, output_file, sample_size=1000, random_seed=4
 if __name__ == "__main__":
     sample_size = 1000
     seed = 42
-    input_file = "Data/LawStackExchange/ExamData/claude_gcp_2024103117/exam.json"
-    output_file = f"Data/LawStackExchange/ExamData/claude_gcp_2024103117/exam_{sample_size}_{seed}.json"
+    input_file = "Data/SecFilings/ExamData/claude_gcp_2024103117/exam.json"
+    output_file = f"Data/SecFilings/ExamData/claude_gcp_2024103117/exam_{sample_size}_{seed}.json"
     sample_json_entries(input_file, output_file)
     
