e906fee36e9141eaada52b584a4db14015a98045
kyosek <kyosuke1029@icloud.com>
2024-11-03 10:31:30 +0000

    adding arxiv exam

--- auto-rag-eval/ExamEvaluator/exam_sampler.py
+++ auto-rag-eval/ExamEvaluator/exam_sampler.py
@@ -39,7 +39,7 @@ def sample_json_entries(input_file, output_file, sample_size=1000, random_seed=4
 if __name__ == "__main__":
     sample_size = 1000
     seed = 42
-    input_file = "Data/StackExchange/ExamData/claude_gcp_2024103016/exam.json"
-    output_file = f"Data/StackExchange/ExamData/claude_gcp_2024103016/exam_{sample_size}_{seed}.json"
+    input_file = "Data/Arxiv/ExamData/claude_gcp_2024103108/exam.json"
+    output_file = f"Data/Arxiv/ExamData/claude_gcp_2024103108/exam_{sample_size}_{seed}.json"
     sample_json_entries(input_file, output_file)
     
