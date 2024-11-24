9274ff8fc7e9a41bdd719bb06d28a5aa760ae784
Kyosuke <kyosuke1029@icloud.com>
2024-11-24 21:25:19 +0000

    add results

--- auto-rag-eval/MultiHopData/Solver/solve_exam_open.py
+++ auto-rag-eval/MultiHopData/Solver/solve_exam_open.py
@@ -170,18 +170,15 @@ if __name__ == "__main__":
     # model_names = ["claude-3-5-sonnet@20240620", "claude-3-5-haiku@20241022"]
     # model_names = ["claude-3-5-sonnet@20240620"]
     model_names = [
-        # 'llama_3_2_3b',
-        'llama_3_1_8b',
+        'llama_3_2_3b',
+        # 'llama_3_1_8b',
         "ministral-8b",
-        "gemma2-9b"
+        # "gemma2-9b",
+        "gemma2-27b",
         ]
     
     exam_files = [
-        # "llama_3_1_8b_single_hop_exam_cleaned_1000_42.json",
-        # "llama_3_2_3b_single_hop_exam_cleaned_1000_42.json",
-        "llama_3_2_3b_exam_cleaned_1000_42.json",
-        "llama_3_1_8b_exam_cleaned_1000_42.json",
-        "gemma2_9b_exam_cleaned_1000_42.json",
+        "exam_new_llama_3_2_3b_processed.json",
         ]
 
     for exam_file in exam_files:
