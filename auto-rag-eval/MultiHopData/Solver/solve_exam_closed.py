00fa11667ebf2b30965b47d4675d6b5b1a5b2e5d
Kyosuke <kyosuke1029@icloud.com>
2024-11-30 22:18:50 +0000

    single hop results

--- auto-rag-eval/MultiHopData/Solver/solve_exam_closed.py
+++ auto-rag-eval/MultiHopData/Solver/solve_exam_closed.py
@@ -264,9 +264,10 @@ if __name__ == "__main__":
         ]
     
     exam_files = [
-        "llama_3_2_3b_single_hop_exam_processed.json",
+        # "llama_3_2_3b_single_hop_exam_processed.json",
         # "gemma2_9b_single_hop_exam_processed.json",
-        "exam_new_ministral_8b_processed_v2.json",
+        "ministral_8b_single_hop_exam_processed.json",
+        # "exam_new_ministral_8b_processed_v2.json",
         # "exam_new_llama_3_2_3b_processed_v2.json",
         # "exam_new_gemma2_9b_processed_v2.json",
         # "exam_new_llama_3_2_3b_processed_v2_unfiltered.json",
