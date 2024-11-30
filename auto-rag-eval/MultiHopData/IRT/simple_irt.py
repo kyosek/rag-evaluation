26035a6f9eb0b3d53be0db20c77a6b1778df8bfe
Kyosuke <kyosuke1029@icloud.com>
2024-11-30 21:22:01 +0000

    single hop results

--- auto-rag-eval/MultiHopData/IRT/simple_irt.py
+++ auto-rag-eval/MultiHopData/IRT/simple_irt.py
@@ -9,7 +9,7 @@ from py_irt.training import IrtModelTrainer
 # Load your exam results from JSON
 with open('auto-rag-eval/MultiHopData/gov_report/exam_results/llama_3_1_8b_open_gemma2_9b_single_hop_exam_processed.json.json', 'r') as f:
     data_single = json.load(f)
-with open('auto-rag-eval/MultiHopData/gov_report/exam_results/llama_3_1_8b_open_exam_new_gemma2_9b_processed_v2.json.json', 'r') as f:
+with open('auto-rag-eval/MultiHopData/gov_report/exam_results/ministral-8b_open_exam_new_gemma2_9b_processed_v2.json.json', 'r') as f:
     data_multi = json.load(f)
 
 # exam_data = data_multi + data_single
