bac42f33838a3dafe1f363f37189decb21d907f1
Kyosuke <kyosuke1029@icloud.com>
2024-12-01 17:06:19 +0000

    process exams

--- auto-rag-eval/MultiHopData/Solver/solve_exam_closed.py
+++ auto-rag-eval/MultiHopData/Solver/solve_exam_closed.py
@@ -244,8 +244,8 @@ def main(task_domain: str, model_type: str, model_name: str, exam_file: str):
 
 
 if __name__ == "__main__":
-    task_domains = ["gov_report", "hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
-    # task_domains = ["hotpotqa", "multifieldqa_en", "SecFilings"]
+    # task_domains = ["gov_report", "hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
+    task_domains = ["hotpotqa", "multifieldqa_en"]
     # model_type = "claude"
     # model_type = "gemini"
     model_type = "cpp"
@@ -266,10 +266,10 @@ if __name__ == "__main__":
     exam_files = [
         # "llama_3_2_3b_single_hop_exam_processed.json",
         # "gemma2_9b_single_hop_exam_processed.json",
-        "ministral_8b_single_hop_exam_processed.json",
-        # "exam_new_ministral_8b_processed_v2.json",
+        # "ministral_8b_single_hop_exam_processed.json",
+        "exam_new_ministral_8b_processed_v2.json",
         # "exam_new_llama_3_2_3b_processed_v2.json",
-        # "exam_new_gemma2_9b_processed_v2.json",
+        "exam_new_gemma2_9b_processed_v2.json",
         # "exam_new_llama_3_2_3b_processed_v2_unfiltered.json",
         # "exam_new_gemma2_9b_processed_v2_unfiltered.json",
         
