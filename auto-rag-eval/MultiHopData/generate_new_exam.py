90e9ffde1cf35e869820b3a48c428e8619b9aff2
Kyosuke <kyosuke1029@icloud.com>
2024-12-02 09:18:59 +0000

    adding ministral sec exam

--- auto-rag-eval/MultiHopData/generate_new_exam.py
+++ auto-rag-eval/MultiHopData/generate_new_exam.py
@@ -613,7 +613,7 @@ if __name__ == "__main__":
     assert sample_size < target_hop_number * 4
     
     # task_domains = ["gov_report", "hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
-    task_domains = ["multifieldqa_en"]
+    task_domains = ["SecFilings"]
     
     # model_names = ["llama_3_2_3b", "gemma2_9b", 'ministral_8b']
     model_names = ["ministral_8b"]
