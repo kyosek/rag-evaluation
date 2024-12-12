a517a45014824e1ead482b291048b81cb8a89b11
Kyosuke <kyosuke1029@icloud.com>
2024-12-12 17:49:16 +0000

    rag single mini

--- auto-rag-eval/MultiHopData/Solver/solve_exam_rag.py
+++ auto-rag-eval/MultiHopData/Solver/solve_exam_rag.py
@@ -232,17 +232,16 @@ if __name__ == "__main__":
     retriever_types = ["Dense", "Sparse"]
     model_names = [
         # 'llama_3_1_8b',
-        # "ministral-8b",
-        "gemma2-9b",
+        "ministral-8b",
+        # "gemma2-9b",
     ]
     exam_files = [
-        # "llama_3_2_3b_single_hop_exam_processed.json",
+        "llama_3_2_3b_single_hop_exam_processed.json",
         # "gemma2_9b_single_hop_exam_processed.json",
         # "ministral_8b_single_hop_exam_processed.json",
-        "exam_new_llama_3_2_3b_processed_v3.json",
+        # "exam_new_llama_3_2_3b_processed_v3.json",
         # "exam_new_gemma2_9b_processed_v2.json",
-        "exam_new_ministral_8b_processed_v3.json",
-        "llama_3_2_3b_single_hop_exam_processed.json",
+        # "exam_new_ministral_8b_processed_v3.json",
     ]
     n_documents = 5
 
