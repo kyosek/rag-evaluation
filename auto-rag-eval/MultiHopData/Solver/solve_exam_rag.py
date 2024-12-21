bdafaea6ed4c9a029b0f9bc1f013c4d199975218
Kyosuke <kyosuke1029@icloud.com>
2024-12-21 14:39:02 +0000

    run gemma single v4

--- auto-rag-eval/MultiHopData/Solver/solve_exam_rag.py
+++ auto-rag-eval/MultiHopData/Solver/solve_exam_rag.py
@@ -226,13 +226,13 @@ def main(
 if __name__ == "__main__":
     # Configuration
     model_type = "cpp"
-    task_domains = ["gov_report", "hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
-    # task_domains = ["SecFilings"]
+    # task_domains = ["gov_report", "hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
+    task_domains = ["hotpotqa", "multifieldqa_en", "SecFilings", "wiki"]
     # retriever_types = ["Dense", "Sparse", "Hybrid", "Rerank"]
     retriever_types = ["Dense"]
     model_names = [
-        'llama_3_1_8b',
-        "ministral-8b",
+        # 'llama_3_1_8b',
+        # "ministral-8b",
         "gemma2-9b",
     ]
     exam_files = [
@@ -244,12 +244,12 @@ if __name__ == "__main__":
         # "exam_new_gemma2_9b_processed_v2.json",
         # "exam_new_ministral_8b_processed_v3.json",
         # V4 (HybridChunk)
-        # "llama_3_2_3b_single_hop_exam_processed_v4.json",
+        "llama_3_2_3b_single_hop_exam_processed_v4.json",
         # "gemma2_9b_single_hop_exam_processed_v4.json",
         # "ministral_8b_single_hop_exam_processed_v4.json",
         # "exam_new_llama_3_2_3b_processed_v4.json",
-        "exam_new_gemma2_9b_processed_v4.json",
-        "exam_new_ministral_8b_processed_v4.json",
+        # "exam_new_gemma2_9b_processed_v4.json",
+        # "exam_new_ministral_8b_processed_v4.json",
         # V5 (chunk: 512)
         # "llama_3_2_3b_single_hop_exam_processed_v5.json",
         # "gemma2_9b_single_hop_exam_processed_v5.json",
